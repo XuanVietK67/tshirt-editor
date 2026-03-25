14c352c35320af7c35b6a09c3c60a7cdd2becbcf
XuanVietK67 <phamxuanviet1492004@gmail.com>
2026-03-25 23:22:59 +0700

    fix bug mobile on shopify layout

--- tshirt-v3/app/routes/app.merchant-portal.tsx
+++ tshirt-v3/app/routes/app.merchant-portal.tsx
@@ -2,7 +2,7 @@ export default function VuePage() {
   return (
     <div style={{ position: 'fixed', inset: 0 }}>
       <iframe
-        src="http://localhost:5173"
+        src="/vue/index.html"
         style={{
           width: '100%',
           height: '100%',
